45995a8d7cc62901d20189884b5988924fb00005
Wagner Medeiros <wmedeiros@Wagners-MBP.TG4ASUS>
2019-11-06 15:16:42 -0300

    Correções para Heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -60,4 +60,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-server.listen(3333);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
